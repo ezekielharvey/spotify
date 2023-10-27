8283f5e20877e400c3688fb219de1dac8f62494d
ezekielharvey <zekeharvey@outlook.com>
2023-10-27 13:37:21 -0400

    Upload Modal and Functionality completeed

---
+++ providers/UserProvider.tsx
@@ -0,0 +1,20 @@
+
+"use client"
+
+import { MyUserContextProvider } from "@/hooks/useUser"
+
+interface UserProviderProps {
+  children: React.ReactNode
+}
+
+const UserProvider: React.FC<UserProviderProps> = ({
+  children
+}: UserProviderProps) => {
+  return (
+    <MyUserContextProvider>
+      {children}
+    </MyUserContextProvider>
+  )
+}
+
+export default UserProvider;
